3368b8d44a6e929ce878a562af7a3e4effd96b54
valentin-ruiz <valentinruiz82290gmail.com>
2025-10-27 16:23:44 +0100

    FEAT: create file2''s function  that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul - multiples two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
