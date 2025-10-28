c83c2126317ef2bcec1d05a8067b88115e9fa58e
valentin-ruiz <valentinruiz82290gmail.com>
2025-10-28 11:13:58 +0100

    FEAT: create file5's function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
